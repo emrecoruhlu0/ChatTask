7a8e7d29d659e125d7d037ca39e7a459a3c0bd7f
emre_coruhlu <ecoruhlu0@gmail.com>
2025-08-27 15:35:54 +0300

    oldu mu sence

---
+++ ChatTask/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatAppServer.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace ChatAppServer.WebAPI.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+}
